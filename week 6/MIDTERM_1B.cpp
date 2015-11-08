8aa2759f37e46c49e8bf93b99ce58ed6a2ecd021
Michael Lowry <mtl6@njit.edu>
2015-11-08 16:38:47 -0500

    Solved Midterm 1b
    Solved midterm C++ implementation of Point structure.

--- week 6/MIDTERM_1B.cpp
+++ week 6/MIDTERM_1B.cpp
@@ -29,3 +29,54 @@ The program contains the following Accessor member functions:
 "	
 
 */
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+struct Point
+{
+	private:
+		
+		int X,Y; //Position
+		float R; //radial distance from Origin
+		
+	public:
+		
+		//mutator and accessor function prototypes for Struct Point
+		void setData(int u, int v)
+		{
+			X = u;
+			Y = v;
+			R = sqrt(u*u + v*v);
+		};
+		float getRadius()const{return R;};
+		void ScalePoint(int scale){X *= scale; Y *= scale; R = sqrt(X*X + Y*Y); };
+		void printPoint()const{	cout << "The Point values are: \nX = " << X << "\tY = " << Y << "\tR = " << R << endl;};
+};
+
+int main()
+{
+	//create point P1 where X = 3, Y = 4
+	Point P1;
+	P1.setData(3,4);
+	//output the Radius of P1
+	P1.printPoint();
+	//Scale P1 by 5
+	P1.ScalePoint(5);
+	//Output the scaled radius of P1
+	P1.printPoint();
+}
+
+/*
+RESULTS: 
+
+The Point values are:
+X = 3   Y = 4   R = 5
+The Point values are:
+X = 15  Y = 20  R = 25
+
+--------------------------------
+Process exited after 0.01267 seconds with return value 44
+Press any key to continue . . .
+*/
